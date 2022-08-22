ca1f8fc490f21291e7b25f9dcf33e8e0b547a73b
Felipe Silva <ffelipe.ssilva02@gmail.com>
2022-08-22 09:34:02 -0300

    primeiro

---
+++ Desktop/Projetos/maven/projetopicos/src/main/java/br/cov/sp/fatec/projetopicos/ProjetopicosApplication.java
@@ -0,0 +1,13 @@
+package br.cov.sp.fatec.projetopicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetopicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetopicosApplication.class, args);
+	}
+
+}
---
+++ Desktop/Projetos/maven/projetopicos/src/test/java/br/cov/sp/fatec/projetopicos/ProjetopicosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.cov.sp.fatec.projetopicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetopicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
